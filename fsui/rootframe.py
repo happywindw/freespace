c648797c615c056dbceb1b90d11e2b50e588b334
happywindw <592595468@qq.com>
2018-05-17 22:53:09 +0800

    rebuild the user face with form builder

--- fsui/rootframe.py
+++ fsui/rootframe.py
@@ -1,69 +1,109 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Jan 23 2018)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
 import wx
 import wx.xrc
 
 
+###########################################################################
+## Class RootFrame
+###########################################################################
+
 class RootFrame(wx.Frame):
 
     def __init__(self, parent):
-        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"FREE SPACE", pos=wx.DefaultPosition,
-                          style=wx.MAXIMIZE | wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"FreeSpace", pos=wx.DefaultPosition, size=wx.Size(-1, -1),
+                          style=wx.DEFAULT_FRAME_STYLE | wx.MAXIMIZE | wx.TAB_TRAVERSAL)
 
-        # self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
-
-        self.CreateStatusBar()
-
-        # Create and set menu and menu items
-        self.menu_file = wx.Menu()
-        self.mi_add_video = wx.MenuItem(self.menu_file, wx.ID_ANY, 'Add Video...', 'Add a single video', wx.ITEM_NORMAL)
-        self.mi_add_video_folder = wx.MenuItem(self.menu_file, wx.ID_ANY, 'Add Video Folder...',
-                                               'Add all videos in the folder and sub folders', wx.ITEM_NORMAL)
-        self.menu_file.Append(self.mi_add_video)
-        self.menu_file.Append(self.mi_add_video_folder)
-        self.menu_file.AppendSeparator()
-
-        self.menu_help = wx.Menu()
-        self.mi_about = wx.MenuItem(self.menu_help, wx.ID_ANY, 'About', 'Show information about FreeSpace',
-                                    wx.ITEM_NORMAL)
-        self.menu_help.Append(self.mi_about)
-
-        # Create and set a menu bar
-        self.menu_bar = wx.MenuBar()
-        self.menu_bar.Append(self.menu_file, 'File')
-        self.menu_bar.Append(self.menu_help, 'Help')
-        self.SetMenuBar(self.menu_bar)
+        self.SetSizeHints(wx.Size(800, 600), wx.DefaultSize)
 
         root_sizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.top_panel = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-        self.top_panel.SetMaxSize(wx.Size(-1, 100))
-        root_sizer.Add(self.top_panel, 1, wx.EXPAND | wx.ALL, 5)
+        self.root_list_book = wx.Listbook(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LB_TOP)
+        self.home_panel = wx.Panel(self.root_list_book, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+        self.root_list_book.AddPage(self.home_panel, u"Home", False)
+        self.movie_panel = wx.Panel(self.root_list_book, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                    wx.TAB_TRAVERSAL)
+        movie_sizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.notebook = wx.Notebook(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0)
-        self.main_page_panel = wx.Panel(self.notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-        self.notebook.AddPage(self.main_page_panel, u"Main page", False)
-        self.movie_page_panel = wx.Panel(self.notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-        self.notebook.AddPage(self.movie_page_panel, u"Movie page", False)
-        self.video_page_panel = wx.Panel(self.notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-        self.notebook.AddPage(self.video_page_panel, u"Video page", False)
+        self.movie_notebook = wx.Notebook(self.movie_panel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                          wx.NB_FIXEDWIDTH | wx.NB_LEFT)
+        self.rider_panel = wx.Panel(self.movie_notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                    wx.TAB_TRAVERSAL)
+        self.movie_notebook.AddPage(self.rider_panel, u"Rider", True)
+        self.saber_panel = wx.Panel(self.movie_notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                    wx.TAB_TRAVERSAL)
+        self.movie_notebook.AddPage(self.saber_panel, u"Saber", False)
+        self.lancer_panel = wx.Panel(self.movie_notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                     wx.TAB_TRAVERSAL)
+        self.movie_notebook.AddPage(self.lancer_panel, u"Lancer", False)
 
-        root_sizer.Add(self.notebook, 1, wx.EXPAND | wx.ALL, 5)
+        movie_sizer.Add(self.movie_notebook, 1, wx.EXPAND | wx.ALL, 5)
+
+        self.movie_panel.SetSizer(movie_sizer)
+        self.movie_panel.Layout()
+        movie_sizer.Fit(self.movie_panel)
+        self.root_list_book.AddPage(self.movie_panel, u"Movie", True)
+        self.video_panel = wx.Panel(self.root_list_book, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                    wx.TAB_TRAVERSAL)
+        self.root_list_book.AddPage(self.video_panel, u"Video", False)
+        self.cartoon_panel = wx.Panel(self.root_list_book, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                      wx.TAB_TRAVERSAL)
+        self.root_list_book.AddPage(self.cartoon_panel, u"Cartoon", False)
+        self.comic_panel = wx.Panel(self.root_list_book, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                    wx.TAB_TRAVERSAL)
+        self.root_list_book.AddPage(self.comic_panel, u"Comic", False)
+
+        root_sizer.Add(self.root_list_book, 1, wx.EXPAND | wx.ALL, 0)
+
+        self.SetSizer(root_sizer)
+        self.Layout()
+        root_sizer.Fit(self)
+        self.status_bar = self.CreateStatusBar(1, wx.STB_SIZEGRIP, wx.ID_ANY)
+        self.menu_bar = wx.MenuBar(0)
+        self.m_video = wx.Menu()
+        self.mm_add_movie = wx.MenuItem(self.m_video, wx.ID_ANY, u"Add Movie...", u"Add a single movie", wx.ITEM_NORMAL)
+        self.m_video.Append(self.mm_add_movie)
+
+        self.mm_add_folder = wx.MenuItem(self.m_video, wx.ID_ANY, u"Add Movie Folder...",
+                                         u"Add all movies in the folder and sub folders", wx.ITEM_NORMAL)
+        self.m_video.Append(self.mm_add_folder)
+
+        self.menu_bar.Append(self.m_video, u"Movie")
+
+        self.m_help = wx.Menu()
+        self.mh_about = wx.MenuItem(self.m_help, wx.ID_ANY, u"About", u"Show information about FreeSpace",
+                                    wx.ITEM_NORMAL)
+        self.m_help.Append(self.mh_about)
+
+        self.menu_bar.Append(self.m_help, u"Help")
+
+        self.SetMenuBar(self.menu_bar)
 
         self.Centre(wx.BOTH)
 
         # Connect Events
-        self.Bind(wx.EVT_MENU, self.mf_add_video, id=self.mi_add_video.GetId())
-        self.Bind(wx.EVT_MENU, self.mf_add_video_folder, id=self.mi_add_video_folder.GetId())
-        self.Bind(wx.EVT_MENU, self.mh_about, id=self.mi_about.GetId())
+        self.Bind(wx.EVT_MENU, self.on_mm_add_movie, id=self.mm_add_movie.GetId())
+        self.Bind(wx.EVT_MENU, self.on_mm_add_movie_folder, id=self.mm_add_folder.GetId())
+        self.Bind(wx.EVT_MENU, self.on_mh_about, id=self.mh_about.GetId())
 
     def __del__(self):
         pass
 
-    # Virtual event handlers, override them in derived class
-    def mf_add_video(self, event):
+    # Virtual event handlers, overide them in your derived class
+    def on_mm_add_movie(self, event):
         event.Skip()
 
-    def mf_add_video_folder(self, event):
+    def on_mm_add_movie_folder(self, event):
         event.Skip()
 
-    def mh_about(self, event):
+    def on_mh_about(self, event):
         event.Skip()
+
+
--- mainwindow.py
+++ mainwindow.py
@@ -7,12 +7,17 @@ class MainWindow(RootFrame):
         super().__init__(parent)
         pass
 
-    def mh_about(self, event):
+    def on_mh_about(self, event):
         wx.MessageBox('Hello, welcome to free space!')
 
-    def mf_add_video(self, event):
+    def on_mm_add_movie(self, event):
         fd = wx.FileDialog(self, 'Add a Single Video File')
         if fd.ShowModal() == wx.ID_OK:
             print('ssss')
         fd.Destroy()
+
+    def on_mm_add_movie_folder(self, event):
+        print('here')
+        pass
+
 
