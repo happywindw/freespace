9db8dbd4703fba95e0d5058c3f956d83bfcb456b
happywindw <592595468@qq.com>
2018-05-20 17:46:15 +0800

    make the main frame outline

--- core/mainwindow.py
+++ core/mainwindow.py
@@ -5,6 +5,15 @@ from fsui.rootframe import RootFrame
 class MainWindow(RootFrame):
     def __init__(self, parent):
         super().__init__(parent)
+        rider_pic_list = [self.rcp_pic_01, self.rcp_pic_02, self.rcp_pic_03, self.rcp_pic_04, self.rcp_pic_05,
+                          self.rcp_pic_06, self.rcp_pic_07, self.rcp_pic_08, self.rcp_pic_09, self.rcp_pic_10,
+                          self.rcp_pic_11, self.rcp_pic_12, self.rcp_pic_13, self.rcp_pic_14, self.rcp_pic_15,
+                          self.rcp_pic_16, self.rcp_pic_17, self.rcp_pic_18, self.rcp_pic_19, self.rcp_pic_20,
+                          self.rcp_pic_21, self.rcp_pic_22, self.rcp_pic_23, self.rcp_pic_24, self.rcp_pic_25,
+                          self.rcp_pic_26, self.rcp_pic_27, self.rcp_pic_28, self.rcp_pic_29, self.rcp_pic_30]
+        for pic in rider_pic_list:
+            bitmap = wx.Bitmap('./resource/test.jpg', wx.BITMAP_TYPE_JPEG)
+            pic.SetBitmap(bitmap)
         pass
 
     def on_mh_about(self, event):
--- fsui/rootframe.py
+++ fsui/rootframe.py
@@ -21,7 +21,7 @@ class RootFrame(wx.Frame):
         wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"FreeSpace", pos=wx.DefaultPosition, size=wx.Size(-1, -1),
                           style=wx.DEFAULT_FRAME_STYLE | wx.MAXIMIZE | wx.TAB_TRAVERSAL)
 
-        self.SetSizeHints(wx.Size(1200, 800), wx.DefaultSize)
+        self.SetSizeHints(wx.Size(1200, 800), wx.Size(1900, 1000))
 
         root_sizer = wx.BoxSizer(wx.VERTICAL)
 
@@ -35,6 +35,214 @@ class RootFrame(wx.Frame):
                                           wx.NB_FIXEDWIDTH | wx.NB_TOP)
         self.rider_panel = wx.Panel(self.movie_notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
                                     wx.TAB_TRAVERSAL)
+        rider_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.rider_splitter = wx.SplitterWindow(self.rider_panel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                                wx.SP_3D)
+        self.rider_splitter.SetSashGravity(0)
+        self.rider_splitter.Bind(wx.EVT_IDLE, self.rider_splitterOnIdle)
+
+        self.rider_left_panel = wx.Panel(self.rider_splitter, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                         wx.TAB_TRAVERSAL)
+        wSizer1 = wx.WrapSizer(wx.HORIZONTAL)
+
+        self.m_button9 = wx.Button(self.rider_left_panel, wx.ID_ANY, u"MyButton", wx.DefaultPosition, wx.DefaultSize, 0)
+        wSizer1.Add(self.m_button9, 0, wx.ALL, 5)
+
+        self.m_button10 = wx.Button(self.rider_left_panel, wx.ID_ANY, u"MyButton", wx.DefaultPosition, wx.DefaultSize,
+                                    0)
+        wSizer1.Add(self.m_button10, 0, wx.ALL, 5)
+
+        self.rider_left_panel.SetSizer(wSizer1)
+        self.rider_left_panel.Layout()
+        wSizer1.Fit(self.rider_left_panel)
+        self.rider_right_panel = wx.Panel(self.rider_splitter, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                          wx.TAB_TRAVERSAL)
+        rrp_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.rider_content_panel = wx.Panel(self.rider_right_panel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                            wx.TAB_TRAVERSAL)
+        bSizer16 = wx.BoxSizer(wx.VERTICAL)
+
+        self.rcp_page_panel = wx.Panel(self.rider_content_panel, wx.ID_ANY, wx.DefaultPosition, wx.Size(-1, 50),
+                                       wx.TAB_TRAVERSAL)
+        self.rcp_page_panel.SetMaxSize(wx.Size(-1, 50))
+
+        rcp_page_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.rcp_text = wx.StaticText(self.rcp_page_panel, wx.ID_ANY,
+                                      u"There are 0 movies in total, 0 pages in total.          Jump to page: ",
+                                      wx.DefaultPosition, wx.DefaultSize, 0)
+        self.rcp_text.Wrap(-1)
+        rcp_page_sizer.Add(self.rcp_text, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.rcp_text_ctrl = wx.TextCtrl(self.rcp_page_panel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition,
+                                         wx.DefaultSize, 0)
+        rcp_page_sizer.Add(self.rcp_text_ctrl, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.rcp_jump_btn = wx.Button(self.rcp_page_panel, wx.ID_ANY, u"Go", wx.DefaultPosition, wx.DefaultSize, 0)
+        rcp_page_sizer.Add(self.rcp_jump_btn, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.rcp_staticline = wx.StaticLine(self.rcp_page_panel, wx.ID_ANY, wx.DefaultPosition, wx.Size(-1, 50),
+                                            wx.LI_VERTICAL)
+        rcp_page_sizer.Add(self.rcp_staticline, 0, wx.ALIGN_CENTER | wx.ALL | wx.EXPAND, 5)
+
+        self.rcp_pageup_btn = wx.Button(self.rcp_page_panel, wx.ID_ANY, u"PageUp", wx.DefaultPosition, wx.DefaultSize,
+                                        0)
+        rcp_page_sizer.Add(self.rcp_pageup_btn, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.rcp_pagedown_btn = wx.Button(self.rcp_page_panel, wx.ID_ANY, u"PageDown", wx.DefaultPosition,
+                                          wx.DefaultSize, 0)
+        rcp_page_sizer.Add(self.rcp_pagedown_btn, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.rcp_page_panel.SetSizer(rcp_page_sizer)
+        self.rcp_page_panel.Layout()
+        bSizer16.Add(self.rcp_page_panel, 1, wx.EXPAND | wx.ALL, 5)
+
+        self.rcp_scrolled_window = wx.ScrolledWindow(self.rider_content_panel, wx.ID_ANY, wx.DefaultPosition,
+                                                     wx.DefaultSize, wx.HSCROLL | wx.VSCROLL)
+        self.rcp_scrolled_window.SetScrollRate(5, 20)
+        rcp_pic_sizer = wx.GridSizer(5, 6, 0, 0)
+
+        self.rcp_pic_01 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_01, 0, wx.ALL, 5)
+
+        self.rcp_pic_02 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_02, 0, wx.ALL, 5)
+
+        self.rcp_pic_03 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_03, 0, wx.ALL, 5)
+
+        self.rcp_pic_04 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_04, 0, wx.ALL, 5)
+
+        self.rcp_pic_05 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_05, 0, wx.ALL, 5)
+
+        self.rcp_pic_06 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_06, 0, wx.ALL, 5)
+
+        self.rcp_pic_07 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_07, 0, wx.ALL, 5)
+
+        self.rcp_pic_08 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_08, 0, wx.ALL, 5)
+
+        self.rcp_pic_09 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_09, 0, wx.ALL, 5)
+
+        self.rcp_pic_10 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_10, 0, wx.ALL, 5)
+
+        self.rcp_pic_11 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_11, 0, wx.ALL, 5)
+
+        self.rcp_pic_12 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_12, 0, wx.ALL, 5)
+
+        self.rcp_pic_13 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_13, 0, wx.ALL, 5)
+
+        self.rcp_pic_14 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_14, 0, wx.ALL, 5)
+
+        self.rcp_pic_15 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_15, 0, wx.ALL, 5)
+
+        self.rcp_pic_16 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_16, 0, wx.ALL, 5)
+
+        self.rcp_pic_17 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_17, 0, wx.ALL, 5)
+
+        self.rcp_pic_18 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_18, 0, wx.ALL, 5)
+
+        self.rcp_pic_19 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_19, 0, wx.ALL, 5)
+
+        self.rcp_pic_20 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_20, 0, wx.ALL, 5)
+
+        self.rcp_pic_21 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_21, 0, wx.ALL, 5)
+
+        self.rcp_pic_22 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_22, 0, wx.ALL, 5)
+
+        self.rcp_pic_23 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_23, 0, wx.ALL, 5)
+
+        self.rcp_pic_24 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_24, 0, wx.ALL, 5)
+
+        self.rcp_pic_25 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_25, 0, wx.ALL, 5)
+
+        self.rcp_pic_26 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_26, 0, wx.ALL, 5)
+
+        self.rcp_pic_27 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_27, 0, wx.ALL, 5)
+
+        self.rcp_pic_28 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_28, 0, wx.ALL, 5)
+
+        self.rcp_pic_29 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_29, 0, wx.ALL, 5)
+
+        self.rcp_pic_30 = wx.BitmapButton(self.rcp_scrolled_window, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition,
+                                          wx.Size(250, 356), wx.BU_AUTODRAW)
+        rcp_pic_sizer.Add(self.rcp_pic_30, 0, wx.ALL, 5)
+
+        self.rcp_scrolled_window.SetSizer(rcp_pic_sizer)
+        self.rcp_scrolled_window.Layout()
+        rcp_pic_sizer.Fit(self.rcp_scrolled_window)
+        bSizer16.Add(self.rcp_scrolled_window, 1, wx.EXPAND | wx.ALL, 5)
+
+        self.rider_content_panel.SetSizer(bSizer16)
+        self.rider_content_panel.Layout()
+        bSizer16.Fit(self.rider_content_panel)
+        rrp_sizer.Add(self.rider_content_panel, 1, wx.EXPAND | wx.ALL, 5)
+
+        self.rider_right_panel.SetSizer(rrp_sizer)
+        self.rider_right_panel.Layout()
+        rrp_sizer.Fit(self.rider_right_panel)
+        self.rider_splitter.SplitVertically(self.rider_left_panel, self.rider_right_panel, 300)
+        rider_sizer.Add(self.rider_splitter, 1, wx.ALIGN_CENTER | wx.EXPAND, 0)
+
+        self.rider_panel.SetSizer(rider_sizer)
+        self.rider_panel.Layout()
+        rider_sizer.Fit(self.rider_panel)
         self.movie_notebook.AddPage(self.rider_panel, u"Rider", True)
         self.saber_panel = wx.Panel(self.movie_notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
                                     wx.TAB_TRAVERSAL)
@@ -42,6 +250,9 @@ class RootFrame(wx.Frame):
         self.lancer_panel = wx.Panel(self.movie_notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
                                      wx.TAB_TRAVERSAL)
         self.movie_notebook.AddPage(self.lancer_panel, u"Lancer", False)
+        self.caster_panel = wx.Panel(self.movie_notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                     wx.TAB_TRAVERSAL)
+        self.movie_notebook.AddPage(self.caster_panel, u"Caster", False)
 
         movie_sizer.Add(self.movie_notebook, 1, wx.EXPAND | wx.ALL, 0)
 
@@ -102,5 +313,9 @@ class RootFrame(wx.Frame):
 
     def on_mh_about(self, event):
         event.Skip()
+
+    def rider_splitterOnIdle(self, event):
+        self.rider_splitter.SetSashPosition(300)
+        self.rider_splitter.Unbind(wx.EVT_IDLE)
 
 
