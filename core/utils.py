674f78d1035fe67d709321f01b1d35efffc153e8
happywindw <592595468@qq.com>
2018-06-26 11:45:34 +0800

    modify the method of resize picture

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,5 @@
 import os
 import wx
-from PIL import Image
 from moviepy.video.io.ffmpeg_reader import ffmpeg_parse_infos
 
 
@@ -24,36 +23,13 @@ def get_fitted_bitmap(pic, size, direction='center'):
         img = img.Scale(int(ogl_size[0] * size[1] / ogl_size[1]), size[1], wx.IMAGE_QUALITY_HIGH)
     if direction == 'center':
         bitmap = wx.Bitmap(img, wx.BITMAP_TYPE_JPEG)
-    elif direction == 'left':
+    elif direction == 'left' or direction == 'top':
         bitmap = wx.Bitmap(img.Resize(size, 0, 0), wx.BITMAP_TYPE_JPEG)
-    elif direction == 'right':
-        bitmap = wx.Bitmap(img.Resize(size, wx.Point(size[0] - img.GetSize()[0], 0)), wx.BITMAP_TYPE_JPEG)
-    elif direction == 'top':
-        bitmap = wx.Bitmap(img.Resize(size, 0, 0), wx.BITMAP_TYPE_JPEG)
-    elif direction == 'bottom':
+    elif direction == 'right' or direction == 'bottom':
         bitmap = wx.Bitmap(img.Resize(size, wx.Point(size[0] - img.GetSize()[0], 0)), wx.BITMAP_TYPE_JPEG)
     else:
-        raise Exception
+        raise Exception("The parameter 'direction' should be one of 'center', 'left', 'right', 'top' or 'bottom'.")
     return bitmap
-
-
-def get_resize_picture(direction, size, pic_file):
-    """
-    resize and cut a picture that matches the given direction and size
-    :param direction: left or right
-    :param size: cut size
-    :param pic_file: picture file
-    :return:
-    """
-    if not os.path.exists(pic_file):
-        return
-    im = Image.open(pic_file)
-    pic_size = im.size
-    res_img = im.resize((int(size[1] / pic_size[1] * pic_size[0]), size[1]), Image.ANTIALIAS)
-    if direction == 'left':
-        region = (0, 0, size[0], size[1])
-        res_img = res_img.crop(region)
-    return res_img
 
 
 def get_duration_definition(video_name):
